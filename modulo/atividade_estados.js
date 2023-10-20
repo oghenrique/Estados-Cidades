af3306113ffafcd288b18f42d5986054c1c97af2
Gustavo <santosguhenrique@gmail.com>
2023-10-20 11:06:03 -0300

    iniciando projeto

---
+++ modulo/atividade_estados.js
@@ -0,0 +1,24 @@
+/************************************************************************************************
+ * Objetivo: Criar uma estrutura de reptição para trazer informações sobre os estados do Brasil. 
+ * Autor: Gustavo Henrique
+ * Data: 20/10/2023
+ * Versão: 1.0
+************************************************************************************************/
+
+var atividadeEstados = require('./estados_cidades.js')
+
+const getListaDeEstados = () => {
+    let estados = pais.estadoCidades.estados
+    let siglas = {}
+    let uf = []
+
+    estados.forEach(function(estados){
+    uf.push(estados.sigla)
+})
+
+    siglas.uf = uf
+    siglas.quantidade = pais.estadoCidades.estados.length
+    console.log(siglas)
+
+    return siglas
+}
