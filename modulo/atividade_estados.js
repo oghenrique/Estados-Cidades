b5bb69a333123fa31482afbf02f354337b473fd7
oghenrique <santosguhenrique@gmail.com>
2023-11-09 22:44:53 -0300

    finalizado

--- modulo/atividade_estados.js
+++ modulo/atividade_estados.js
@@ -6,12 +6,12 @@
 ************************************************************************************************/
 var estadosBrasil = require('./estados_cidades.js')
 
-const getListaDeEstados = function() {
+const getListaDeEstados = function () {
     let estados = estadosBrasil.estadosCidades.estados
     let siglas = {}
     let uf = []
 
-    estados.forEach(function(estado) {
+    estados.forEach(function (estado) {
         uf.push(estado.sigla)
     })
 
@@ -22,30 +22,30 @@ const getListaDeEstados = function() {
     return siglas
 }
 
-const getDadosEstado = function(siglaEstado){
+const getDadosEstado = function (siglaEstado) {
     let sigla = siglaEstado.toUpperCase()
     let estados = estadosBrasil.estadosCidades.estados
     let dadosEstado = {}
 
-    estados.forEach(function(estado){
-        if (estado.sigla.includes(sigla)){
+    estados.forEach(function (estado) {
+        if (estado.sigla.includes(sigla)) {
             dadosEstado.uf = estado.sigla
             dadosEstado.descricao = estado.nome
             dadosEstado.capital = estado.capital
             dadosEstado.regiao = estado.regiao
         }
     })
-     console.log(dadosEstado)
-     return dadosEstado
+    console.log(dadosEstado)
+    return dadosEstado
 }
 
-const getCapitalEstado = function(siglaEstado){
+const getCapitalEstado = function (siglaEstado) {
     let sigla = siglaEstado.toUpperCase()
     let capitais = estadosBrasil.estadosCidades.estados
     let dadosCapital = {}
 
-    capitais.forEach(function (estadosBrasil){
-        if(estadosBrasil.sigla.includes(sigla)){
+    capitais.forEach(function (estadosBrasil) {
+        if (estadosBrasil.sigla.includes(sigla)) {
             dadosCapital.uf = estadosBrasil.sigla
             dadosCapital.descricao = estadosBrasil.nome
             dadosCapital.capital = estadosBrasil.capital
@@ -56,11 +56,94 @@ const getCapitalEstado = function(siglaEstado){
 
 }
 
-const getEstadosRegiao
+const getEstadosRegiao = function (regiaoSelecionada) {
+    const regiao = regiaoSelecionada.toUpperCase()
+
+    const estados = estadosBrasil.estadosCidades.estados
+
+    let infoRegiao = {}
+    infoRegiao.regiao = regiao
+
+    let estadosDaRegiao = []
+
+    estados.forEach(function (estado) {
+        if (estado.regiao.toUpperCase().includes(regiao)) {
+            let infoEstado = {
+                uf: estado.sigla,
+                descricao: estado.nome,
+            };
+
+            estadosDaRegiao.push(infoEstado)
+        }
+    })
+
+    infoRegiao.estados = estadosDaRegiao
+
+    return infoRegiao
+}
+
+const getCapitalPais = function () {
+
+    const estados = estadosBrasil.estadosCidades.estados
+
+    let informacoesCapitaisPais = {}
+    let listaCapitais = []
+
+    estados.forEach(function (estado) {
+        if (estado.capital_pais !== undefined) {
+            let infoEstado = {
+                capital_atual: estado.capital_pais.capital,
+                uf: estado.sigla,
+                descricao: estado.nome,
+                capital: estado.capital,
+                regiao: estado.regiao,
+                ano_inicio_capital_pais: estado.capital_pais.ano_inicio,
+                ano_termino_capital_pais: estado.capital_pais.ano_fim
+            }
+
+            listaCapitais.push(infoEstado)
+        }
+    })
+
+    informacoesCapitaisPais.capitais = listaCapitais
+    
+    return informacoesCapitaisPais
+}
+
+
+const getCidades = function(siglaEstado){
+
+    let estados = estadosBrasil.estadosCidades.estados
+
+    let uf = siglaEstado.toUpperCase()
+
+    let estadoCidades = {}
+    let cidades = []
+
+    estados.forEach(function(estado){
+        if(estado.sigla.toUpperCase().includes(uf)){
+            estadoCidades.uf = estado.sigla
+            estadoCidades.descricao = estado.nome
+            estadoCidades.quantidade_cidades = estado.cidades.length
+
+            estado.cidades.forEach(function(cidade) {
+                cidades.push(cidade.nome)
+            })
+        }
+    })
+
+    estadoCidades.cidades = cidades
+    console.log(estadoCidades)
+    return estadoCidades
+}
+
 
 
 
 
 console.log(getCapitalEstado('sp'))
-// getDadosEstado('ba')
-// getListaDeEstados()
+console.log(getEstadosRegiao('sudeste'))
+console.log(getCapitalPais())
+getCidades('ac')
+getDadosEstado('ba')
+getListaDeEstados()
