46cc37263c73cd3700ef43820b4b9d1032d2a80f
Gustavo <santosguhenrique@gmail.com>
2023-11-10 09:32:22 -0300

    editando app

---
+++ app.js
@@ -0,0 +1,16 @@
+/*************************************************************************
+ * Objetivo: Criar uma API para responder dados de Estados e Cidades     *
+ * Autor: Gustavo Henrique                                               *
+ * Data: 10/11/2023                                                      *
+ * Versão: 1.0                                                           *
+**************************************************************************/
+
+/**
+ * Instalações das dependencias para criação da API
+ *  express     -> npm install express --save
+ *      Dependencia do node para auxiliar na criação de API
+ *  cors        -> npm install cors --save
+ *      Dependencia para manipular recursos de acesso, permissões, etc da API
+ *  body-parser -> npm install body-parser --save
+ *      Dependencia para auxiliar na chegada de dados na API
+ */
